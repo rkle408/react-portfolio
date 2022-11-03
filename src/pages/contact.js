187ac7278ec8390b6bb16e7ec09fab8f678e6cef
Rosebud <rkle408@gmail.com>
2022-11-02 22:37:01 -0700

    started Contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactPage (){
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleChange = (event) => {
+        if (event.target.name === "email") {
+        } else {
+            if (event.target.name === "email") {
+                setErrorMessage(`${event.target.name} is required!`)
+            }
+        }
+    }
+
     return (
-        <div>
-            Contact Rosa
-        </div>
+        <section>
+            <h1>Contact Me!</h1>
+            <form id="contact-form">
+                <div>
+                    <label>Name: </label>
+                    <input type="text" name="name" onBlur={handleChange}/>
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p>{errorMessage}</p>
+                    </div>
+                )}
+                <button type="submit">Submit</button>
+            </form>
+        </section>
     )
 };
 
