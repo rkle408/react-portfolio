d956fa5ce7a5f1990ae888e8633a864e757f3218
Rosebud <rkle408@gmail.com>
2022-11-03 10:13:58 -0700

    updated photos on portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./style.css";
+import dibs from "../assets/dibs-homepage.png";
+import gifBoard from "../assets/gif-board-screenshot2.png";
+import workDay from "../assets/work-day-scheduler-screenshot.png";
+import passwordGen from "../assets/password-generator-screenshot.png";
 
 function PortfolioPage (){
     return (
@@ -14,7 +18,7 @@ function PortfolioPage (){
                     <p>As a group, we developed an application that allows you to post things you want to give away, and get items, all for free! We utilized Node, Bootstrap framework, along with jQuery.</p>
                     <br/>
                     <figure id="gif-board-img">
-                        <a href="https://intense-brook-23504.herokuapp.com/" target="_blank" rel="noreferrer"><img src="./assets/images/screencapture11.png" alt="Screenshot - homepage of our marketplace" /></a>
+                        <a href="https://intense-brook-23504.herokuapp.com/" target="_blank" rel="noreferrer"><img className="portfolioPics" src={dibs} alt="Screenshot - homepage of our marketplace" /></a>
                     </figure>
                     <a className="githubRepo" href="https://github.com/rkle408/dibs" target="_blank" rel="noreferrer">Dibs GitHub Repo</a>
                 </div>
@@ -28,7 +32,7 @@ function PortfolioPage (){
                     <p>In my first project working with a team, together, we developed an application that required the use of two APIs. We decided to bring a little joy to your day by making a random word generator (or you could select your own word!) that also generates an associated gif! Warning: it may not be completely kid-friendly! We utilized the Bulma framework, along with jQuery.</p>
                     <br/>
                     <figure id="gif-board-img">
-                    <a href="https://rkle408.github.io/gif-board/" target="_blank" rel="noreferrer"><img src="./assets/images/gif-board-screenshot2.png" alt="Screenshot of home page of random word and gif generator application" /></a>
+                    <a href="https://rkle408.github.io/gif-board/" target="_blank" rel="noreferrer"><img className="portfolioPics" src={gifBoard} alt="Screenshot of home page of random word and gif generator application" /></a>
                     </figure>
                     <a className="githubRepo" href="https://github.com/rkle408/gif-board" target="_blank" rel="noreferrer">Gif Board GitHub Repo</a>
                 </div>
@@ -41,7 +45,7 @@ function PortfolioPage (){
                 <div class="message-body">
                     <p>Utilizing JavaScript and jQuery, I developed an interactive one-day work scheduler that invokes local storage to keep your reminders.</p>
                     <figure id="work-day-scheduler-img">
-                            <a href="https://rkle408.github.io/work-day-scheduler/" target="_blank" rel="noreferrer"><img src="./assets/images/work-day-scheduler-screenshot.png" alt="Screenshot of work day scheduler/planner application" /></a>
+                            <a href="https://rkle408.github.io/work-day-scheduler/" target="_blank" rel="noreferrer"><img className="portfolioPics" src={workDay} alt="Screenshot of work day scheduler/planner application" /></a>
                     </figure>
                     <a className="githubRepo" href="https://github.com/rkle408/work-day-scheduler" target="_blank" rel="noreferrer">Work Day Scheduler GitHub Repo</a>
                 </div>
@@ -54,7 +58,7 @@ function PortfolioPage (){
                 <div>
                     <p>Do you use the same password for everything? Well, fear not, I've built the perfect application for you to generate a random (... and probably easily-forgettable password! Just tell us what your preferences are!</p>
                     <figure id="password-generator-img">
-                    <a href="https://rkle408.github.io/password-generator/" target="_blank" rel="noreferrer"><img src="./assets/images/password-generator-screenshot.png" alt="Screenshot of random password generator"/></a>
+                    <a href="https://rkle408.github.io/password-generator/" target="_blank" rel="noreferrer"><img className="portfolioPics" src={passwordGen} alt="Screenshot of random password generator"/></a>
                     </figure>
                     <a className="githubRepo" href="https://github.com/rkle408/password-generator" target="_blank" rel="noreferrer">Password Generator GitHub Repo</a>
                 </div>
