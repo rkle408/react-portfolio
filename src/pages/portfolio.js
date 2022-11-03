217bc2467bed7f089f17ded324ac8eee961369b7
Rosebud <rkle408@gmail.com>
2022-11-03 01:49:29 -0700

    added border to each project

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -6,7 +6,7 @@ function PortfolioPage (){
         <section className="paragraph">
             <h1 className="H1">Rosa's Portfolio</h1>
 
-            <article>
+            <article className="border">
                 <div id="dibs">
                     <h2>Project "dibs"</h2>
                 </div>
@@ -16,11 +16,11 @@ function PortfolioPage (){
                     <figure id="gif-board-img">
                         <a href="https://intense-brook-23504.herokuapp.com/" target="_blank" rel="noreferrer"><img src="./assets/images/screencapture11.png" alt="Screenshot - homepage of our marketplace" /></a>
                     </figure>
-                    <a href="https://github.com/rkle408/dibs" target="_blank" rel="noreferrer">Dibs GitHub Repo</a>
+                    <a className="githubRepo" href="https://github.com/rkle408/dibs" target="_blank" rel="noreferrer">Dibs GitHub Repo</a>
                 </div>
             </article><br/>
 
-            <article >
+            <article className="border">
                 <div class="message-header" id="gif-board">
                     <h2 >Project "gif-board"</h2>
                 </div>
@@ -30,11 +30,11 @@ function PortfolioPage (){
                     <figure id="gif-board-img">
                     <a href="https://rkle408.github.io/gif-board/" target="_blank" rel="noreferrer"><img src="./assets/images/gif-board-screenshot2.png" alt="Screenshot of home page of random word and gif generator application" /></a>
                     </figure>
-                    <a href="https://github.com/rkle408/gif-board" target="_blank" rel="noreferrer">Gif Board GitHub Repo</a>
+                    <a className="githubRepo" href="https://github.com/rkle408/gif-board" target="_blank" rel="noreferrer">Gif Board GitHub Repo</a>
                 </div>
             </article><br/>
 
-            <article>
+            <article className="border">
                 <div class="message-header">
                           <h2>Project "work-day-scheduler"</h2>
                 </div>
@@ -43,20 +43,20 @@ function PortfolioPage (){
                     <figure id="work-day-scheduler-img">
                             <a href="https://rkle408.github.io/work-day-scheduler/" target="_blank" rel="noreferrer"><img src="./assets/images/work-day-scheduler-screenshot.png" alt="Screenshot of work day scheduler/planner application" /></a>
                     </figure>
-                    <a href="https://github.com/rkle408/work-day-scheduler" target="_blank" rel="noreferrer">Work Day Scheduler GitHub Repo</a>
+                    <a className="githubRepo" href="https://github.com/rkle408/work-day-scheduler" target="_blank" rel="noreferrer">Work Day Scheduler GitHub Repo</a>
                 </div>
             </article><br/>
 
-            <article>
+            <article className="border">
                 <div>
                     <h2>Project "password-generator"</h2>
                 </div>
                 <div>
                     <p>Do you use the same password for everything? Well, fear not, I've built the perfect application for you to generate a random (... and probably easily-forgettable password! Just tell us what your preferences are!</p>
-                    <figure class="image is-5by3" id="password-generator-img">
+                    <figure id="password-generator-img">
                     <a href="https://rkle408.github.io/password-generator/" target="_blank" rel="noreferrer"><img src="./assets/images/password-generator-screenshot.png" alt="Screenshot of random password generator"/></a>
                     </figure>
-                    <a href="https://github.com/rkle408/password-generator" target="_blank" rel="noreferrer">Password Generator GitHub Repo</a>
+                    <a className="githubRepo" href="https://github.com/rkle408/password-generator" target="_blank" rel="noreferrer">Password Generator GitHub Repo</a>
                 </div>
             </article><br/>
 
